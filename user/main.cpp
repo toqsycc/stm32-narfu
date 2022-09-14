171e814af5dd3dcbdc2969f332ab8d7108392d0d
toqsycc <einhorn.sv@gmail.com>
2022-09-14 14:10:26 +0300

    war crimes

--- user/main.cpp
+++ user/main.cpp
@@ -11,6 +11,18 @@ void ledInit()
 	HAL_GPIO_Init(GPIOD, &boardLED);
 }
 
+void btnInit()
+{
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	GPIO_InitTypeDef btn;
+	btn.Mode = GPIO_MODE_INPUT;
+	btn.Pull = GPIO_PULLDOWN;
+	btn.Pin = GPIO_PIN_0;
+
+	HAL_GPIO_Init(GPIOA, &btn);
+}
+
 void delayMs(volatile int ms)
 {
 	int j;
@@ -22,18 +34,80 @@ void delayMs(volatile int ms)
 
 int main()
 {
+	uint32_t count = 0;
+	uint8_t isReadyToRead = 0xFF;
+	//uint8_t status[4] = { 0 };
+	btnInit();
 	ledInit();
+
+	//HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
 
 	for(;;) 
 	{ 
-		HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
-		delayMs(40);
-		HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
-		delayMs(40);
-		HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
-		delayMs(40);
-		HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
-		delayMs(40);
+		/*
+		if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) && !(count % 2))
+		{
+			count++;
+			for (uint8_t i = 0; i < 4; i++)
+			{
+				if (!status[i])
+				{
+					status[i] = !status[i];
+					HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12 + i);
+				}
+				else continue;
+			}
+			delayMs(40);
+		}
+
+		else if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) && count % 2)
+		{
+			count++;
+			for (uint8_t i = 0; i < 4; i++)
+			{
+				if (status[i])
+				{
+					status[i] = !status[i];
+					HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12 + i);
+				}
+				else continue;
+			}
+			delayMs(40);
+		}*/
+
+		if (!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)) isReadyToRead = 0xFF;
+
+		if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) && !(count % 4) && isReadyToRead)
+		{
+			isReadyToRead = 0x00;
+			count++;
+			HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+			delayMs(40);
+		}
+
+		else if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) && !((count % 4) - 1) && isReadyToRead)
+		{
+			isReadyToRead = 0x00;
+			count++;
+			HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
+			delayMs(40);
+		}
+
+		else if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) && !((count % 4) - 2) && isReadyToRead)
+		{
+			isReadyToRead = 0x00;
+			count++;
+			HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
+			delayMs(40);
+		}
+
+		else if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) && !((count % 4) - 3) && isReadyToRead)
+		{
+			isReadyToRead = 0x00;
+			count++;
+			HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
+			delayMs(40);
+		}
 	}
 
 	return 0;
